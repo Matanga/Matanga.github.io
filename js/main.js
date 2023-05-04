c8f334caf203ecbb9fef96488b2cb1db0ba8b312
Pablo Vezzini <sublime51@gmail.com>
2023-05-04 14:41:52 -0300

    .

--- js/main.js
+++ js/main.js
@@ -109,7 +109,7 @@ function OpenTab(evt, tabName) {
   }
 
   // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
+  tablinks = document.getElementsByClassName("dropbtn");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
